23989b1bff1276fcf8a90591ecc5ff7230064016
Chao Hsuan Chi <allen0413@gmail.com>
2017-12-06 09:59:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
-  if array.length == 2
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
     array.join(" and ")
-  elsif
-    array.join(", ")
+  else
+    "#{array[0, array.length].join(', ')} and #{array.last}"
   end
 end
